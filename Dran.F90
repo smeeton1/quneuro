da2557dae00106cb318a04084f74cfa11051b01c
smeeton1 <smeeton1>
2017-11-22 21:54:43 -0600

    added some notes

--- Dran.F90
+++ Dran.F90
@@ -1,5 +1,15 @@
+!! check jump operators for measuremnet
+!! for learning teach bell coherant and noon statescheck time against classical
+!! also see about impliementation against qnot gates
+!! check starting with thermal distrubution for qbits.
+
 
 program dran
 
 
+allocate phi(n,2)
+
+
+
 end program
+
