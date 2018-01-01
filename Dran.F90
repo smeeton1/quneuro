92aa744215d97b891f31ce7f17327ae1656e2c31
smeeton1 <smeeton1>
2017-12-31 23:18:42 -0600

    added subroutines set up

--- Dran.F90
+++ Dran.F90
@@ -3,11 +3,32 @@
 !! also see about impliementation against qnot gates
 !! check starting with thermal distrubution for qbits.
 
+subroutine swap(phi)
+
+end subroutine
+
+subroutine mix(phi)
+
+end subroutine
+
+subroutine qinter(phi,qphi)
+
+end subroutine
+
+subroutine qsole(qphi)
+
+end subroutine
+
+subroutine qjump(qphi)
+
+end subroutine
 
 program dran
 
 
 allocate phi(n,2)
+
+
 
 
 
