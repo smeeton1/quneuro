b157ac73030223e2596c5d7406d4471391055c2c
smeeton1 <smeeton1>
2018-01-02 10:19:46 -0600

    writing quantum walk functions for a line

--- Dran.F90
+++ Dran.F90
@@ -4,10 +4,34 @@
 !! check starting with thermal distrubution for qbits.
 
 subroutine swap(phi)
+ complex,dimention(:,:),intent(inout)::phi
+ int:: i,n
+ complex:: swap
+ n=size(phi,1)
+ do i=2,n-1
+   swap = phi(i,2)
+   phi(i,2)=phi(i+1,1)
+   phi(i+1,1)=swap
+   swap=phi(i,1)
+   phi(i,1)=phi(i-1,2)
+   phi(i-1,2)=swap
+ enddo
 
 end subroutine
 
 subroutine mix(phi)
+ complex,dimention(:,:),intent(inout)::phi
+ complex,dimention(2,2)::Coin
+ complex::hold
+ int:: i,n
+ n=size(phi,1)
+ Coin(1,1)=1/sqrt(2);Coin(1,2)=1/sqrt(2);Coin(2,1)=1/sqrt(2);Coin(2,2)=-1/sqrt(2)
+ do i=2,n-1
+   hold=phi(i,1)
+   phi(i,1)=1/sqrt(2)*hold+1/sqrt(2)*phi(i,2)
+   phi(i,2)=1/sqrt(2)*hold-1/sqrt(2)*phi(i,2)
+ enddo
+ 
 
 end subroutine
 
