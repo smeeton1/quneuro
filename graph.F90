53424b99b805ea482674abf9066a32cb8dd97734
smeeton1 <smeeton1>
2018-08-29 12:09:23 -0500

    added start for jump and graphics

---
+++ graph.F90
@@ -0,0 +1,19 @@
+module graph
+  implicit none
+  
+contains 
+
+
+
+
+
+end module
+
+
+program g_test
+use graph
+implicit none
+
+
+
+end program
---
+++ jump.F90
@@ -0,0 +1,22 @@
+module jump
+  implicit none
+  
+contains 
+
+subroutine jmes(Q)
+
+
+end subroutine
+
+
+
+end module
+
+
+program j_test
+use jump
+implicit none
+
+
+
+end program
