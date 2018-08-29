f5f056d18adf2eeb00f5b214902f28a286540b43
smeeton1 <smeeton1>
2018-08-29 12:03:51 -0500

    got denisty working need jst jump owrking now

--- density.F90
+++ density.F90
@@ -1,15 +1,16 @@
 module density
   implicit none
   
-  
- subroutine dens_build(a,b,D)
-  complex(kdp),dimension(:),intent(inout)    :: D
-  complex(kdp),dimension(:),intent(in)       :: a,b
-  integer                                    :: i,j,k
+contains 
+ 
+subroutine dens_build(a,b,D)
+  complex,dimension(:),intent(inout)    :: D
+  complex,dimension(:),intent(in)       :: a,b
+  integer                               :: i,j,k,n,m
   
   n=size(a)
   m=size(b)
-  allocate(D(n*m))
+  
   k=1
   do i=1,n
     do j=1,m
@@ -19,13 +20,13 @@ module density
   enddo
   
   
- end subroutine
+end subroutine
   
   
  subroutine k_product(A,B,C)
-  complex(kdp),dimension(:,:),intent(inout)  :: C
-  complex(kdp),dimension(:,:),intent(in)     :: A,B
-  integer                                    :: i,j,k,l,n,m
+  complex,dimension(:,:),intent(inout)  :: C
+  complex,dimension(:,:),intent(in)     :: A,B
+  integer                               :: i,j,k,l,n,m
   
   n=size(A,1)
   m=size(C,1)
@@ -45,9 +46,9 @@ module density
   
   
  subroutine par_traceA(A,D)!set to work for the case A and B are the same size
-  complex(kdp),dimension(:),intent(inout)  :: A
-  complex(kdp),dimension(:),intent(in)     :: D
-  integer                                  :: i,j,k,l,n,m
+  complex,dimension(:),intent(inout)  :: A
+  complex,dimension(:),intent(in)     :: D
+  integer                             :: i,j,k,l,n,m
   
   n=size(D)
   m=size(A)
@@ -64,18 +65,18 @@ module density
   
   
  subroutine par_traceB(B,D)!set to work for the case A and B are the same size
-  complex(kdp),dimension(:),intent(inout)  :: B
-  complex(kdp),dimension(:),intent(in)     :: D
-  integer                                  :: i,j,k,l,n,m
+  complex,dimension(:),intent(inout)  :: B
+  complex,dimension(:),intent(in)     :: D
+  integer                             :: i,j,k,l,n,m
   
   n=size(B)
   m=size(D)
   l=m/n
   B(:)=cmplx(0,0)
   
-  do i=1,l
-    do j=1,n
-      B(j)=B(j)+D(i+(j-1)*n)
+  do i=1,n
+    do j=1,l
+      B(i)=B(i)+D((j-1)*n+i)
      enddo
   enddo
   
@@ -87,11 +88,11 @@ program K_test
 use density
 implicit none
 
-complex, dimension(:,:), allocatable:: phi,qphi
+complex, dimension(:,:), allocatable:: phi,qphi,C
 complex, dimension(:), allocatable:: D,a
 integer::n,i,j
 
-n=10
+n=2
 allocate(phi(n,2))
 allocate(qphi(n,2))
 phi(:,:)=cmplx(0.0,0.0)
@@ -99,13 +100,36 @@ qphi(:,:)=cmplx(0.0,0.0)
 do i=1,n
   qphi(i,1)=cmplx(1.0,0.0)
   do j=1,2
-    phi(i,j)=1/sqrt(2.0)
+    phi(i,j)=j/real(10)*sqrt(2.0)
   enddo
 enddo
+allocate(D(4))
+OPEN(10, file='out.dat', status='REPLACE')
+allocate(C(n*n,n*n))
+call k_product(phi,qphi,C)
 
+write(10,*)phi(1,1),phi(1,2)
+write(10,*)phi(2,1),phi(2,2)
+write(10,*)' '
+write(10,*)qphi(1,1),qphi(1,2)
+write(10,*)qphi(2,1),qphi(2,2)
+write(10,*)' '
+write(10,*)C(1,1),C(1,2),C(1,3),C(1,4)
+write(10,*)C(2,1),C(2,2),C(2,3),C(2,4)
+write(10,*)C(3,1),C(3,2),C(3,3),C(3,4)
+write(10,*)C(4,1),C(4,2),C(4,3),C(4,4)
+write(10,*)' '
+
+
+write(10,*)phi(1,:),qphi(1,:)
 call dens_build(phi(1,:),qphi(1,:),D)
+call par_traceA(phi(1,:),D)
+call par_traceB(qphi(1,:),D)
+write(10,*)phi(1,:),qphi(1,:)
+write(10,*)D
+write(10,*)' '
 
-call
 
+close(10)
 
 end program
