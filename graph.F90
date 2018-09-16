3959544ef8fb89d5e8c2fd444b5dc1c3df5db5d6
smeeton1 <smeeton1>
2018-09-16 10:10:04 -0500

    did most of thr graphics

--- graph.F90
+++ graph.F90
@@ -8,36 +8,89 @@ contains
 !draw line by node 
 
 subroutine clear()
+integer   :: i
 
-end subroutine
-
-
-subroutine draw_line(phi,open_node,n)
-
-end subroutine
-
-subroutine draw_graph(phi,A,open_node,n)
+do i=0,50
+  write(*,*)' '
+enddo
 
 end subroutine
 
 subroutine draw_qubit(open_node,n)
+logical,dimension(:),intent(in)    :: open_node
+integer,intent(in)                 :: n
+integer                            :: i
+
+write(*,'(A)', ADVANCE = "NO")'      '
+
 do i =1,n
   if(open_node(i))then
-    write(*,'(A)', ADVANCE = "NO")'[32m =   '
+    write(*,'(A)', ADVANCE = "NO")'=   '
   else
-    write(*,'(A)', ADVANCE = "NO")'[31m =   '
+    write(*,'(A)', ADVANCE = "NO")'=   '
   endif
 enddo
 
 end subroutine
 
-subroutine DrawOpenNode()
-write(*,'(A)', ADVANCE = "NO")'[97m O---'
+subroutine Drawline(open_node,n)
+logical,dimension(:),intent(in)    :: open_node
+integer,intent(in)                 :: n
+integer                            :: i
+
+write(*,'(A)', ADVANCE = "NO")'      '
+
+do i =1,n
+  if(open_node(i))then
+    write(*,'(A)', ADVANCE = "NO")'O---'
+  else
+    write(*,'(A)', ADVANCE = "NO")'O   '
+  endif
+enddo
+    
+end subroutine
+
+
+subroutine draw_lineg(phi,open_node,n)
+complex,dimension(:,:),intent(in)  :: phi
+logical,dimension(:),intent(in)    :: open_node
+integer,intent(in)                 :: n
+integer                            :: i
+
+call clear()
+
+call draw_qubit(open_node,n)
+
+write(*,*)' ' 
+
+call Drawline(open_node,n)
+
+do i=0,6
+  write(*,*)' '
+enddo
 
 end subroutine
 
-subroutine DrawClosedNode()
-write(*,'(A)', ADVANCE = "NO")'[97m O   '
+subroutine draw_graph(phi,A,open_node,n)
+complex,dimension(:,:),intent(in)  :: phi
+integer,dimension(:,:),intent(in)  :: A
+logical,dimension(:),intent(in)    :: open_node
+integer,intent(in)                 :: n
+integer                            :: i
+
+
+call clear()
+
+call draw_qubit(open_node,n)
+
+write(*,*)' ' 
+
+call Drawline(open_node,n)
+
+do i=0,6
+  write(*,*)' '
+enddo
+
 
 end subroutine
 
@@ -49,6 +102,25 @@ end module
 program g_test
 use graph
 implicit none
+logical,dimension(:),allocatable  :: open_node
+integer                           :: n
+
+n=4
+
+allocate(open_node(n))
+
+open_node(:)=.TRUE.
+open_node(3)=.false.
+
+call clear()
+
+call draw_qubit(open_node,n)
+
+write(*,*)' ' 
+
+call Drawline(open_node,n)
+
+write(*,*)' '
 
 
 
