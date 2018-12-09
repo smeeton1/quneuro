253e4991270c2a18583de9f063a3df0f337646b5
smeeton1 <jeremy.p.rodriguez@mail.mcgill.ca>
2018-12-09 15:25:20 -0600

    got nre interaction function working

--- jump.F90
+++ jump.F90
@@ -55,22 +55,23 @@ end subroutine
 subroutine qbit_ev(qbit,phi,s,dt)
   complex*16,dimension(:),intent(inout) :: qbit,phi
   real,intent(in)                       :: dt,s
-  complex*8                             :: g,hold,hold2
+  complex*16                            :: hold,hold2
   integer                               :: l,i
+  real*8                                :: g
 
 
 l=size(phi)
-g=cmplx(0.0,0.0)
+g=0.0
 do i=1,l
-  g=g+phi(i)
+  g=g+abs(real(phi(i)*conjg(phi(i))))
 enddo
 
-if(abs(real(g*conjg(g))).gt.0.00001)then
+if(abs(g).gt.0.00001)then
   hold=qbit(1)
   hold2=qbit(2)
 
-  qbit(1)=cos(s*g*dt)!*hold
-  qbit(2)=(hold2/abs(hold2*hold2))*sqrt(((cos(s*g*dt))**2-1)*abs(hold*hold))
+  qbit(1)=cos(s*g*dt)+hold!*hold
+  qbit(2)=(hold2/abs(hold2*hold2))*sqrt((1-(cos(s*g*dt))**2)*abs(hold*hold))
 else
   hold=qbit(1)
   hold2=qbit(2)
@@ -79,32 +80,36 @@ else
   qbit(2)=(hold2/abs(hold2*hold2))*sqrt(1-(exp(-2.0*dt))*abs(hold*hold))
 endif
 
+if(real(qbit(1)*conjg(qbit(1))+qbit(2)*conjg(qbit(2))).gt.1.0)then
+  qbit(1)=qbit(1)/sqrt(qbit(1)*conjg(qbit(1))+qbit(2)*conjg(qbit(2)))
+  qbit(2)=qbit(2)/sqrt(qbit(1)*conjg(qbit(1))+qbit(2)*conjg(qbit(2)))
+endif
 
 end subroutine
 
 end module
 
 
-program j_test
-use jump
-!use IFCORE
-implicit none
-complex*16,dimension(:),allocatable :: Q,P
-integer                          :: R
-Logical(4)                       :: S
-
-allocate(Q(2),P(2))
-Q(1)=cmplx(1/sqrt(2.0),0.0)
-Q(2)=cmplx(1/sqrt(2.0),0.0)
-Q(1)=Q(1)/(CONJG(Q(1))*Q(1)+CONJG(Q(2))*Q(2))
-Q(2)=Q(2)/(CONJG(Q(1))*Q(1)+CONJG(Q(2))*Q(2))
-S=.True.
-P(1)=cmplx(0.0,0.0)
-P(2)=cmplx(0.0,0.0)
-Write(*,*)Q(1),' ',Q(2)
-Write(*,*)(CONJG(Q(1))*Q(1)+CONJG(Q(2))*Q(2))
-call qbit_ev(Q,P,10.0,5.0) !jmes(Q,S)
-Write(*,*)Q(1),' ',Q(2)
-Write(*,*)(CONJG(Q(1))*Q(1)+CONJG(Q(2))*Q(2))
-
-end program
+! program j_test
+! use jump
+! !use IFCORE
+! implicit none
+! complex*16,dimension(:),allocatable :: Q,P
+! integer                          :: R
+! Logical(4)                       :: S
+! 
+! allocate(Q(2),P(2))
+! Q(1)=cmplx(1/sqrt(2.0),0.0)
+! Q(2)=cmplx(1/sqrt(2.0),0.0)
+! Q(1)=Q(1)/(CONJG(Q(1))*Q(1)+CONJG(Q(2))*Q(2))
+! Q(2)=Q(2)/(CONJG(Q(1))*Q(1)+CONJG(Q(2))*Q(2))
+! S=.True.
+! P(1)=cmplx(0.0,0.3)
+! P(2)=cmplx(0.0,0.2)
+! Write(*,*)Q(1),' ',Q(2)
+! Write(*,*)(CONJG(Q(1))*Q(1)+CONJG(Q(2))*Q(2))
+! call qbit_ev(Q,P,10.0,5.0) !jmes(Q,S)
+! Write(*,*)Q(1),' ',Q(2)
+! Write(*,*)(CONJG(Q(1))*Q(1)+CONJG(Q(2))*Q(2))
+! 
+! end program
